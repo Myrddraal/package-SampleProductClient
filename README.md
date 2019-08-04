84a56685b74b5dd20f8357bd6d9ec92571fdbd7c
David <david.rj.forster+gh@gmail.com>
2019-08-04 21:17:59 +0100

    Sample client defined using strings
    Using strings instead of the actual contract.
    Included some sample metadata yaml files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# package-SampleClient
+# package-SampleProductClient
 Sample client for CutAndDRY
---
+++ SampleProductClient.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+
+namespace SampleProductClient
+{
+    public class ProductClient
+    {
+        private static readonly HttpClient HttpClient = new HttpClient();
+        private readonly Uri _apiUri;
+
+        public ProductClient(Uri apiUri)
+        {
+            _apiUri = apiUri;
+        }
+
+        public async Task<List<string>> GetAllProducts()
+        {
+            HttpResponseMessage response = await HttpClient.GetAsync(_apiUri);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsAsync<List<string>>();
+        }
+
+        public async Task<string> GetProduct(Guid id)
+        {
+            HttpResponseMessage response = await HttpClient.GetAsync(new Uri(_apiUri, id.ToString()));
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsAsync<string>();
+        }
+
+        public async Task<string> AddProduct(string product)
+        {
+            HttpResponseMessage response = await HttpClient.PostAsync(_apiUri, new ObjectContent<string>(product, new JsonMediaTypeFormatter()));
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsAsync<string>();
+        }
+
+        public async Task DeleteProduct(Guid id)
+        {
+            HttpResponseMessage response = await HttpClient.DeleteAsync(new Uri(_apiUri, id.ToString()));
+
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
